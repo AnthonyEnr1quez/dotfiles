40756ce66409ef958e22442ddc16d2db5ef161d8
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2025-08-22 08:21:19 -0500

    go 1.25.0

--- modules/home-manager/langs/go/default.nix
+++ modules/home-manager/langs/go/default.nix
@@ -11,11 +11,11 @@
   programs = {
     go = {
       enable = true;
-      package = pkgs.go_1_24.overrideAttrs (_: rec {
-        version = "1.24.6";
+      package = pkgs.go_1_25.overrideAttrs (_: rec {
+        version = "1.25.0";
         src = pkgs.fetchurl {
           url = "https://go.dev/dl/go${version}.src.tar.gz";
-          hash = "sha256-4ctVgqq1iGaLwEwH3hhogHD2uMmyqvNh+CHhm9R8/b0=";
+          hash = "sha256-S9AekSlyB7+kUOpA1NWpOxtTGl5DhHOyoG4Y4HciciU=";
         };
       });
       goPath = "go";
