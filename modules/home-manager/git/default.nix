60a3302c44eb67ea12faa31d05b3a45236d0c4a5
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2025-10-27 12:23:54 -0500

    add mergiraf

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -1,49 +1,53 @@
 { config, pkgs, ... }: {
-  programs.git = {
-    enable = true;
+  programs = {
+    git = {
+      enable = true;
 
-    difftastic.enable = true;
+      difftastic.enable = true;
 
-    extraConfig = {
-      commit = {
-        gpgSign = true;
-      };
-      core = {
-        editor = "vim --nofork";
-      };
-      gpg = {
-        format = "ssh";
-        ssh.allowedSignersFile = toString (pkgs.writeText "allowed_signers" "");
-      };
-      init.defaultBranch = "main";
-      pull.ff = "only";
-      push.autoSetupRemote = true;
-      rerere = {
-        autoupdate = true;
-        enabled = true;
-      };
-      url = {
-        "git@github.com:" = {
-          insteadOf = "https://github.com/";
+      extraConfig = {
+        commit = {
+          gpgSign = true;
+        };
+        core = {
+          editor = "vim --nofork";
+        };
+        gpg = {
+          format = "ssh";
+          ssh.allowedSignersFile = toString (pkgs.writeText "allowed_signers" "");
+        };
+        init.defaultBranch = "main";
+        pull.ff = "only";
+        push.autoSetupRemote = true;
+        rerere = {
+          autoupdate = true;
+          enabled = true;
+        };
+        url = {
+          "git@github.com:" = {
+            insteadOf = "https://github.com/";
+          };
         };
       };
+
+      signing = {
+        key = "~/.ssh/id_ed25519_github.pub";
+      };
+
+      ignores = [
+        ".DS_STORE"
+      ];
+
+      aliases = {
+        default-branch = "!git symbolic-ref refs/remotes/origin/HEAD | cut -d '/' -f3,4";
+        com = "!f(){ git checkout $(git default-branch) $@;}; f";
+        last = "log -1 HEAD";
+        aa = "!git add . && git commit --amend --no-edit";
+        amend = "commit --amend";
+        poke = "!(git diff --exit-code --quiet && git diff --cached --exit-code --quiet) && git commit --allow-empty -m 'poke' || echo stash or revert any changes for ur poke";
+      };
     };
 
-    signing = {
-      key = "~/.ssh/id_ed25519_github.pub";
-    };
-
-    ignores = [
-      ".DS_STORE"
-    ];
-
-    aliases = {
-      default-branch = "!git symbolic-ref refs/remotes/origin/HEAD | cut -d '/' -f3,4";
-      com = "!f(){ git checkout $(git default-branch) $@;}; f";
-      last = "log -1 HEAD";
-      aa = "!git add . && git commit --amend --no-edit";
-      amend = "commit --amend";
-      poke = "!(git diff --exit-code --quiet && git diff --cached --exit-code --quiet) && git commit --allow-empty -m 'poke' || echo stash or revert any changes for ur poke";
-    };
+    mergiraf.enable = true;
   };
 }
