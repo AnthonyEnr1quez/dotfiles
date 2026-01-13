1fa0a065ae648a02377b0a7a9aaa7c419ed96fb7
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2026-01-13 12:48:28 -0600

    some more git settings

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -11,6 +11,7 @@
         key = "~/.ssh/id_ed25519_github.pub";
       };
 
+      # https://blog.gitbutler.com/how-git-core-devs-configure-git
       settings = {
         alias = {
           default-branch = "!git symbolic-ref refs/remotes/origin/HEAD | cut -d '/' -f3,4";
@@ -20,23 +21,35 @@
           amend = "commit --amend";
           poke = "!(git diff --exit-code --quiet && git diff --cached --exit-code --quiet) && git commit --allow-empty -m 'poke' || echo stash or revert any changes for ur poke";
         };
+        branch.sort = "-committerdate";
+        column.ui = "auto";
         commit = {
           gpgSign = true;
+          verbose = true;
         };
         core = {
           editor = "vim --nofork";
+        };
+        diff = {
+          algorithm = "histogram";
+          colorMoved = "plain";
+          mnemonicPrefix = true;
+          renames = true;
         };
         gpg = {
           format = "ssh";
           ssh.allowedSignersFile = toString (pkgs.writeText "allowed_signers" "");
         };
+        help.autocorrect = "prompt";
         init.defaultBranch = "main";
+        merge.conflictstyle = "zdiff3";
         pull.ff = "only";
         push.autoSetupRemote = true;
         rerere = {
           autoupdate = true;
           enabled = true;
         };
+        tag.sort = "version:refname";
         url = {
           "git@github.com:" = {
             insteadOf = "https://github.com/";
