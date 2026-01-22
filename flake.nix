867893a6eb6ef63543cb32ecaf07f95b7eb34fc0
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2026-01-22 10:30:06 -0600

    add nix community cache

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   description = "dotfiles";
 
+  # NOTE: Keep these in sync with modules/darwin/default.nix nix.settings
   nixConfig = {
     substituters = [
       "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
       "https://anthonyenr1quez.cachix.org"
     ];
 
     trusted-public-keys = [
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "anthonyenr1quez.cachix.org-1:Gclb+0ZEVse0quS5IhHiYRsb9QgZ7oSPRfKPNHOl3eI="
     ];
   };
