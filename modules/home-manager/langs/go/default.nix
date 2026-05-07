2f6f78449d7afce490230081a2355fb6f3b9bf4b
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2026-05-07 16:27:36 -0500

    go 1.26.3

--- modules/home-manager/langs/go/default.nix
+++ modules/home-manager/langs/go/default.nix
@@ -16,10 +16,10 @@
     go = {
       enable = true;
       package = pkgs.go_1_26.overrideAttrs (_: rec {
-        version = "1.26.2";
+        version = "1.26.3";
         src = pkgs.fetchurl {
           url = "https://go.dev/dl/go${version}.src.tar.gz";
-          hash = "sha256-LpHrtpR6lulDb7KzkmqIAu/mOm03Xf/sT4Kqnb1v1Ds=";
+          hash = "sha256-HGRoddCqh5kTMYTtV895/yS97+jIggRwYCqdPW2Rkrg=";
         };
       });
       env.GOPATH = "${config.home.homeDirectory}/go";
