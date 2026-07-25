f0bd375002b8fdf714337bfeac5ce17dfa55c49f
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2026-07-25 14:21:30 -0500

    pkg order

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -29,8 +29,8 @@
       kubectx
       # kubelogin
       # kubernetes-helm-wrapped
-      nixd
       # mouser TODO, gesture support is somewhat mid as of 06/26, swapped on offline logi-options-+ https://hub.sync.logitech.com/options/post/logi-options-offline-manual-I2a7NSJyE6oH2oy
+      nixd
       # pgadmin4
       # slack
       # unar
