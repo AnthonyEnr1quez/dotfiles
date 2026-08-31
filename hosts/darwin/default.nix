94a78e85746e45e6044d9b750805d7a9e030f000
AnthonyEnr1quez <32233059+AnthonyEnr1quez@users.noreply.github.com>
2026-08-31 13:36:12 -0500

    turn off linux builder

--- hosts/darwin/default.nix
+++ hosts/darwin/default.nix
@@ -8,7 +8,7 @@
 
   # Off by default: the agent-sandbox VM closure is built in CI and pulled from
   # cachix. Flip to true temporarily to (re)build the VM locally, then back.
-  microvm.linuxBuilder.enable = true;
+  microvm.linuxBuilder.enable = false;
 
   hm = {
     firefox = {
